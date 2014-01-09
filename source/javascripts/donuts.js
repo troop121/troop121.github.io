2931a9498fb291d2fdd23b4bdbfe4c102e56c973
Adam L. Coyne <adam@coyne.nu>
2014-01-09 13:45:10 -0600

    Remove console.log

--- source/javascripts/donuts.js
+++ source/javascripts/donuts.js
@@ -29,7 +29,7 @@ donutApp.controller('donutController', function donutController($scope, $http) {
     var url = "http://spreadsheets.google.com/feeds/list/" + options.ssKey + "/" + options.ssSheet + "/public/values?alt=json";
 
     $http.jsonp(url + '&callback=JSON_CALLBACK').success(function(data) {
-        console.log('success:', arguments);
+        //console.log('success:', arguments);
 		$scope.results = [];
         angular.forEach(data, function(value, index){
             angular.forEach(value.entry, function(entry, index){
@@ -37,7 +37,7 @@ donutApp.controller('donutController', function donutController($scope, $http) {
             });
         });
     }).error(function(error) {
-        console.log('error:', arguments);
+        //console.log('error:', arguments);
 		$scope.results = [];
     });
 });
